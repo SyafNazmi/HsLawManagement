59b46f63477522af7778b28db4fb34d5ec89acff
SyafNazmi <syafiepe.cs@student.usm.my>
2023-01-26 18:23:26 +0800

    removing comment in the menu

--- src/Clock_in.java
+++ src/Clock_in.java
@@ -25,12 +25,12 @@ class MainMenu
         System.out.println("\t\t\t     Modified by: Huda, Sofea & Syafie");
         System.out.println("\t\t\t    --------------------");
         System.out.println("\n\nPress 1 : To Add an Employee Details");
-     //   System.out.println("Press 2 : To See an Employee Details ");
-     //   System.out.println("Press 3 : To Remove an Employee");
-      //  System.out.println("Press 4 : To Update Employee Details");
+        System.out.println("Press 2 : To See an Employee Details ");
+        System.out.println("Press 3 : To Remove an Employee");
+        System.out.println("Press 4 : To Update Employee Details");
         System.out.println("Press 2 : To Exit the EMS Portal");
-        System.out.println("Press 3 : To Add A New Case");
-      //  System.out.println("Press 7 : To View Case");
+        System.out.println("Press 3 : To Add A New Client");
+        System.out.println("Press 7 : To View View");
         System.out.println("Press 4 : To Check In and Check Out an Employee");
     }
 
